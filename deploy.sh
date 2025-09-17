2bc8886383cb30777a83a7bf1097d3cdda48ef08
melaveetha <retromeras@ya.ru>
2025-09-17 16:33:09 +0300

    Hotfix [make yarn write to null] deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 echo "Installing dependencies"
-yarn install
+yarn install > /dev/null
 echo "Start to build your package"
-yarn build
+yarn build > /dev/null
 
 echo "Deploying"
 rm -rf /var/www/html/docs
 mkdir -p /var/www/html/docs
 mv ./dist/* /var/www/html/docs/
 
-echo "Finished!
+echo "Finished!"
